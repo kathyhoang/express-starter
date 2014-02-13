ff88a6716bc1355d61558cbae5b2b4cb4997c591
Kathy <kathyhoang8D@gmail.com>
2014-02-13 15:23:06 -0800

    meow

--- static/js/calc.js
+++ static/js/calc.js
@@ -145,7 +145,7 @@ $('#mod').click(function() {
   $('#equals').click(function() {
     var outputText = $('output').text();
     var outputNum = parseFloat(eval(outputText));
-    var res = eval(outputNum);
+    //var newNum = eval(outputNum);
     $('#output').text(newNum);
   });
 
