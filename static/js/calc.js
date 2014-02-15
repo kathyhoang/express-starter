984f06a7f3442c06af2bc3d7b9d845117297a81d
Kathy <kathyhoang8D@gmail.com>
2014-02-14 16:32:43 -0800

    last mother time

--- static/js/calc.js
+++ static/js/calc.js
@@ -145,7 +145,7 @@ $('#mod').click(function() {
   $('#equals').click(function() {
     var outputText = $('output').text();
     var test = '4+8';
-    $('#output').text(eval(outputText));
+    $('#output').text(eval($('output').text()));
   });
 
   $('#dot').click(function() {
